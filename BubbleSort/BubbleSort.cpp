90e1b9d2a045e8a66c65f0af2fd49ca57dc8bcee
Muhammadalif1213 <muhammadalif1213@gmail.com>
2023-03-17 16:35:04 +0700

    membuat prosedur ke 4 untuk menampilkan hasil yang tersusun

--- BubbleSort/BubbleSort.cpp
+++ BubbleSort/BubbleSort.cpp
@@ -46,3 +46,14 @@ void unsorted() {
 	}
 	cout << endl;
 }
+
+void display() {
+	cout << endl;								// output baris kosong
+	cout << "======================================" << endl;	//output ke layar
+	cout << "Element arraay yang telah di tersusun" << endl;		//output ke layar
+	cout << "======================================" << endl;	//output ke layar
+	for (int j = 0; j < n; j++) {				//looping dengan j dimulai dari 0 hingga n-1
+		cout << a[j] << endl;					// output ke layar
+	}
+	cout << endl;								// output baris kosong
+}
