9c8cfe7d22a6a0ab700aef71118fb7fe6946e0c3
Muhammadalif1213 <muhammadalif1213@gmail.com>
2023-03-17 16:20:37 +0700

    menambahkan prosedur ke 2 (mengurutkan array dengan metode bubble sort)

--- BubbleSort/BubbleSort.cpp
+++ BubbleSort/BubbleSort.cpp
@@ -24,3 +24,14 @@ void input() {		 //procedur untuk input
 		cin >> a[i];					// Input dari pengguna
 	}
 }
+void bubbleSortArray() {	// prosedur untuk mengurutkan array dengan metode bubble sort
+	for (int i = 1; i < n; i++) {		// looping dengan i dimulai dari 1 hingga n-1
+		for (int j = 0; j < n - i; j++) {	// looping dengan J dimulai dari 0 hingga n-i
+			if (a[j] > a[j + 1]) {			// jika nilai pada a[j] lebih besar dari a[j+1]
+				int temp = a[j];			// simpan nilai a[j] ke variabel sementara temp
+				a[j] = a[j + 1];			// assign nilai a[j + 1] ke a[j]
+				a[j + 1] = temp;			// assign nilai temp ke a[j + 1]
+			}
+		}
+	}
+}
